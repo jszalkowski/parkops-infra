9389dc107e4c0702a30441337f3e687258a0a6d7
Matt Peter <mpeter@redhat.com>
2016-02-18 13:06:35 -0500

    add golang, use manual updated choco script so we can use toad 7.7

---
+++ packer/scripts/windows/install_packages_toad.ps1
@@ -0,0 +1,20 @@
+﻿$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$packageName = 'toad.mysql'
+$installerType = 'exe'
+$url = 'http://community-downloads.quest.com/toadsoft/MySQL/ToadforMySQL_Freeware_7.7.0.579.zip'
+$url64 = $url 
+$silentArgs = '/S'
+$validExitCodes = @(0)
+
+try {
+  Install-ChocolateyZipPackage "$packageName" "$url" "$toolsDir" "$url64"
+
+  $fileToInstall = Join-Path "$toolsDir" 'ToadforMySQL_Freeware_7.7.0.579.exe'
+
+  Install-ChocolateyInstallPackage "$packageName" "$installerType" "$silentArgs" "$fileToInstall" -validExitCodes $validExitCodes
+
+  Write-ChocolateySuccess "$packageName"
+} catch {
+  Write-ChocolateyFailure "$packageName" "$($_.Exception.Message)"
+  throw 
+}
--- packer/scripts/windows/install_packages_toadexec.ps1
+++ packer/scripts/windows/install_packages_toadexec.ps1
@@ -2,4 +2,4 @@ choco install awscli
 choco install awstools.powershell
 choco install git.install
 choco install nodejs.install
-choco install toad.mysql
+choco install golang
