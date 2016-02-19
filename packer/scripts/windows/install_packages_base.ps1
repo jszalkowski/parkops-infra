41f236a517cf3afc3e1168f90a28ba361ff2f238
Matt Peter <mpeter@redhat.com>
2016-02-19 13:14:48 -0500

    updated windows packages

--- packer/scripts/windows/install_packages_base.ps1
+++ packer/scripts/windows/install_packages_base.ps1
@@ -1,3 +1,8 @@
 choco feature enable -n allowGlobalConfirmation
 choco install nssm
 choco install consul
+choco install unzip
+choco install grep
+choco install wget
+choco install conemu
+choco install kickassvim
--- packer/scripts/windows/install_packages_toadexec.ps1
+++ packer/scripts/windows/install_packages_toadexec.ps1
@@ -3,3 +3,8 @@ choco install awstools.powershell
 choco install git.install
 choco install nodejs.install
 choco install golang
+choco install unzip
+choco install wget
+choco install kickassvim
+choco install conemu
+
